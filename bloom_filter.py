f65b8ac9715a3dc74b43a8514111e904db3fdab6
mrGunar <pasishack372@gmail.com>
2025-09-03 19:03:57 +0300

    Update BloomFilter: Added additional options for size and hash calculations

--- bloom_filter.py
+++ bloom_filter.py
@@ -8,11 +8,42 @@ class BloomFilter:
     Attributes:
         size: An integer identificator of the filter size.
         filter: A bytearray filter that stored information about the entity exists or not.
+        hash_func_count: A number stands for how many times a hash function needs to be called.
     """
 
-    def __init__(self, size: int) -> None:
-        self.size = size
-        self.filter = bytearray([0 for _ in range(size)])
+    def __init__(
+        self,
+        number_of_items: int,
+        false_positive_probability: float = 0.0001,
+    ) -> None:
+        """
+        Args:
+            number_of_items: The number of items to be stored.
+            false_positive_probability: A false positive probability parameter.
+
+        Notes: We need to add dependency of false_positive_probability parameter.
+        """
+        self.size = self._get_size(number_of_items)
+
+        self.hash_func_count = self._get_number_of_hash_functions(
+            self.size, number_of_items
+        )
+
+        self.filter = bytearray([0 for _ in range(self.size)])
+
+    def _get_number_of_hash_functions(self, size: int, item_count: int) -> int:
+        """The method calculate the number of hash functions.
+
+        Notes: At this moment, it simply return 1.
+        """
+        return 1
+
+    def _get_size(self, number_of_items: int) -> int:
+        """This method calculates size of the filter.
+
+        Notes: At this moment we don't use any algorithm to calculate this number.
+        """
+        return number_of_items
 
     def add(self, value: tp.Hashable) -> None:
         """The method adds information to the filter.
